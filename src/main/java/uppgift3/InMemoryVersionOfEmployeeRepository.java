0e74572891c11d8dd7d9bbe46489948716699a84
Milad Nazari <milad.nazari@live.se>
2021-03-25 15:24:02 +0100

    Uppgift 3 Implemented in memory version of EmployeeRepository, and created tests for all methods inside.

---
+++ src/main/java/uppgift3/InMemoryVersionOfEmployeeRepository.java
@@ -0,0 +1,33 @@
+package uppgift3;
+
+import com.example.Employee;
+import com.example.EmployeeRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryVersionOfEmployeeRepository implements EmployeeRepository {
+
+    ArrayList<Employee> employees = new ArrayList<>();
+
+    @Override
+    public List<Employee> findAll() {
+        return employees;
+    }
+
+    @Override
+    public Employee save(Employee e) {
+        if (e.getId().equals("0")) {
+            e.setId((String.valueOf(employees.size() + 1)));
+
+        }
+        for (Employee employee : employees)
+            if (e.getId().equals(employee.getId())) {
+                employee.setSalary(e.getSalary());
+                return e;
+            }
+
+        employees.add(e);
+        return e;
+    }
+}
---
+++ src/test/java/uppgift3/InMemoryVersionOfEmployeeRepositoryTest.java
@@ -0,0 +1,51 @@
+package uppgift3;
+
+import com.example.Employee;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class InMemoryVersionOfEmployeeRepositoryTest {
+
+
+    InMemoryVersionOfEmployeeRepository inMemoryVersionOfEmployeeRepository = new InMemoryVersionOfEmployeeRepository();
+
+    @Test
+    @DisplayName("Has saved employee")
+    void hasSavedEmployee() {
+
+        Employee employee = new Employee("1", 2000);
+        inMemoryVersionOfEmployeeRepository.save(employee);
+
+        assertEquals(1, inMemoryVersionOfEmployeeRepository.findAll().size());
+
+    }
+
+    @Test
+    @DisplayName("Overwriting employee")
+    void overwritingEmployee() {
+
+        Employee employee1 = new Employee("1", 2000);
+        Employee employee2 = new Employee("1", 3000);
+        inMemoryVersionOfEmployeeRepository.save(employee1);
+
+        inMemoryVersionOfEmployeeRepository.save(employee2);
+
+        assertEquals(1, inMemoryVersionOfEmployeeRepository.findAll().size());
+        assertEquals(3000, inMemoryVersionOfEmployeeRepository.findAll().get(0).getSalary());
+
+    }
+
+    @Test
+    @DisplayName("Adding employee with no id")
+    void addingEmployeeWithNoId() {
+
+        Employee employee = new Employee("0", 2000);
+
+        inMemoryVersionOfEmployeeRepository.save(employee);
+
+        assertEquals("1", inMemoryVersionOfEmployeeRepository.findAll().get(0).getId());
+
+    }
+}
